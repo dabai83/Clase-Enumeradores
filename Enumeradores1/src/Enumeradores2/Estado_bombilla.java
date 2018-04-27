3a6431a30d5e9ab7d4ca306fbed76a5e1f5f13be
dabai83 <diegoalberto.agueroibarra@gmail.com>
2018-04-27 10:28:03 -0400

    first commit

---
+++ Enumeradores1/src/Enumeradores2/Estado_bombilla.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enumeradores2;
+
+/**
+ *
+ * @author Albert
+ */
+public enum Estado_bombilla {
+    ON,OFF;
+
+    public static Estado_bombilla getON() {
+        return ON;
+    }
+
+    public static Estado_bombilla getOFF() {
+        return OFF;
+    }
+
+
+}
