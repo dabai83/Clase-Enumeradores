e0ee1fc24b252b0acc84895afc6e13848c94dcab
dabai83 <diegoalberto.agueroibarra@gmail.com>
2018-05-28 10:19:30 -0400

    first commit

---
+++ Abstracion/src/abstracion/datos/AccesoDatos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstracion.datos;
+
+/**
+ *
+ * @author Albert
+ */
+public class AccesoDatos implements manejointerfaces{
+
+    @Override
+    public void Manejointerfaces() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
---
+++ Abstracion/src/abstracion/datos/ImplementacionMySql.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstracion.datos;
+
+/**
+ *
+ * @author Albert
+ */
+public class ImplementacionMySql extends AccesoDatos{
+      @Override
+    public void Manejointerfaces() {
+        super.Manejointerfaces(); //To change body of generated methods, choose Tools | Templates.
+    }  
+}
